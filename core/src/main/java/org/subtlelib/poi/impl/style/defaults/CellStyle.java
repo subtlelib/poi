9a950b4e610c1e98b3bb8d07d9c4de8823196605
Christian Olsson <christian.olsson@opitz-consulting.de>
2018-05-30 07:32:14 +0200

    Upgrade auf Apache POI 3.17

--- core/src/main/java/org/subtlelib/poi/impl/style/defaults/CellStyle.java
+++ core/src/main/java/org/subtlelib/poi/impl/style/defaults/CellStyle.java
@@ -1,42 +1,43 @@
 package org.subtlelib.poi.impl.style.defaults;
 
-import static org.apache.poi.ss.usermodel.CellStyle.BORDER_DOUBLE;
-import static org.apache.poi.ss.usermodel.CellStyle.BORDER_NONE;
-import static org.apache.poi.ss.usermodel.CellStyle.BORDER_THIN;
-
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.subtlelib.poi.api.style.AdditiveStyle;
 import org.subtlelib.poi.api.workbook.WorkbookContext;
 
+import static org.apache.poi.ss.usermodel.BorderStyle.DOUBLE;
+import static org.apache.poi.ss.usermodel.BorderStyle.NONE;
+import static org.apache.poi.ss.usermodel.BorderStyle.THIN;
+
 public enum CellStyle implements AdditiveStyle {
 
-	BORDERS_THIN_ALL(BORDER_THIN, BORDER_THIN, BORDER_THIN, BORDER_THIN),
-	BORDERS_BOTTOM_THIN(BORDER_NONE, BORDER_NONE, BORDER_THIN, BORDER_NONE),
-	BORDERS_TOP_BOTTOM_THIN(BORDER_THIN, BORDER_NONE, BORDER_THIN, BORDER_NONE),
-	BORDERS_BOTTOM_DOUBLE(BORDER_NONE, BORDER_NONE, BORDER_DOUBLE, BORDER_NONE);
+    BORDERS_THIN_ALL(THIN, THIN, THIN, THIN),
+    BORDERS_BOTTOM_THIN(NONE, NONE, THIN, NONE),
+    BORDERS_TOP_BOTTOM_THIN(THIN, NONE, THIN, NONE),
+    BORDERS_BOTTOM_DOUBLE(NONE, NONE, DOUBLE, NONE);
 
-    private final short borderTop;
-    private final short borderRight;
-    private final short borderBottom;
-    private final short borderLeft;
+    private final BorderStyle borderTop;
+    private final BorderStyle borderRight;
+    private final BorderStyle borderBottom;
+    private final BorderStyle borderLeft;
 
-    private CellStyle(short borderTop, short borderRight, short borderBottom, short borderLeft) {
+    CellStyle(BorderStyle borderTop, BorderStyle borderRight, BorderStyle borderBottom, BorderStyle borderLeft) {
         this.borderTop = borderTop;
         this.borderRight = borderRight;
         this.borderBottom = borderBottom;
         this.borderLeft = borderLeft;
     }
 
-	@Override
-	public void enrich(WorkbookContext workbookContext, org.apache.poi.ss.usermodel.CellStyle style) {
-		style.setBorderTop(borderTop);
+    @Override
+    public void enrich(WorkbookContext workbookContext, org.apache.poi.ss.usermodel.CellStyle style) {
+        style.setBorderTop(borderTop);
         style.setBorderRight(borderRight);
         style.setBorderBottom(borderBottom);
         style.setBorderLeft(borderLeft);
     }
 
-	@Override
-	public Enum<StyleType> getType() {
-		return StyleType.CELL;
-	}
+    @Override
+    public Enum<StyleType> getType() {
+        return StyleType.CELL;
+    }
 
 }
--- core/src/main/java/org/subtlelib/poi/impl/style/defaults/FontStyle.java
+++ core/src/main/java/org/subtlelib/poi/impl/style/defaults/FontStyle.java
@@ -1,46 +1,46 @@
 package org.subtlelib.poi.impl.style.defaults;
 
-import static org.apache.poi.ss.usermodel.CellStyle.ALIGN_LEFT;
-import static org.apache.poi.ss.usermodel.CellStyle.ALIGN_RIGHT;
-import static org.apache.poi.ss.usermodel.CellStyle.VERTICAL_BOTTOM;
-import static org.apache.poi.ss.usermodel.Font.BOLDWEIGHT_BOLD;
-import static org.apache.poi.ss.usermodel.Font.BOLDWEIGHT_NORMAL;
-import static org.apache.poi.ss.usermodel.Font.U_NONE;
-import static org.apache.poi.ss.usermodel.Font.U_SINGLE;
-
 import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.subtlelib.poi.api.style.AdditiveStyle;
 import org.subtlelib.poi.api.workbook.WorkbookContext;
 
+import static org.apache.poi.ss.usermodel.Font.U_NONE;
+import static org.apache.poi.ss.usermodel.Font.U_SINGLE;
+import static org.apache.poi.ss.usermodel.HorizontalAlignment.LEFT;
+import static org.apache.poi.ss.usermodel.HorizontalAlignment.RIGHT;
+import static org.apache.poi.ss.usermodel.VerticalAlignment.BOTTOM;
+
 public enum FontStyle implements AdditiveStyle {
     /** All these styles use default font (Calibri for xlsx files, Arial for xls files) */
-	NORMAL(10, BOLDWEIGHT_NORMAL, U_NONE, ALIGN_LEFT, VERTICAL_BOTTOM),
-    ITALIC(10, BOLDWEIGHT_NORMAL, U_NONE, ALIGN_LEFT, VERTICAL_BOTTOM, true),
-	NORMAL_RIGHT(10, BOLDWEIGHT_NORMAL, U_NONE, ALIGN_RIGHT, VERTICAL_BOTTOM),
-	BOLD(10, BOLDWEIGHT_BOLD, U_NONE, ALIGN_LEFT, VERTICAL_BOTTOM),
-	BOLD_RIGHT(10, BOLDWEIGHT_BOLD, U_NONE, ALIGN_RIGHT, VERTICAL_BOTTOM),
+    NORMAL(10, false, U_NONE, LEFT, BOTTOM),
+    ITALIC(10, false, U_NONE, LEFT, BOTTOM, true),
+    NORMAL_RIGHT(10, false, U_NONE, RIGHT, BOTTOM),
+    BOLD(10, true, U_NONE, LEFT, BOTTOM),
+    BOLD_RIGHT(10, true, U_NONE, RIGHT, BOTTOM),
 
-	COLUMN_HEADER(10, BOLDWEIGHT_BOLD, U_NONE, ALIGN_LEFT, VERTICAL_BOTTOM),
-	COLUMN_HEADER_RIGHT(10, BOLDWEIGHT_BOLD, U_NONE, ALIGN_RIGHT, VERTICAL_BOTTOM),
-	SECTION_HEADER(10, BOLDWEIGHT_BOLD, U_SINGLE, ALIGN_LEFT, VERTICAL_BOTTOM),
-	SECTION_HEADER_RIGHT(10, BOLDWEIGHT_BOLD, U_SINGLE, ALIGN_RIGHT, VERTICAL_BOTTOM),
-    PRIMARY_HEADER(12, BOLDWEIGHT_BOLD, U_SINGLE, ALIGN_LEFT, VERTICAL_BOTTOM),
-    SECONDARY_HEADER(14, BOLDWEIGHT_BOLD, U_NONE, ALIGN_LEFT, VERTICAL_BOTTOM);
+    COLUMN_HEADER(10, true, U_NONE, LEFT, BOTTOM),
+    COLUMN_HEADER_RIGHT(10, true, U_NONE, RIGHT, BOTTOM),
+    SECTION_HEADER(10, true, U_SINGLE, LEFT, BOTTOM),
+    SECTION_HEADER_RIGHT(10, true, U_SINGLE, RIGHT, BOTTOM),
+    PRIMARY_HEADER(12, true, U_SINGLE, LEFT, BOTTOM),
+    SECONDARY_HEADER(14, true, U_NONE, LEFT, BOTTOM);
 
-    private final short height;
-    private final short boldWeight;
-    private final byte underline;
-    private final short horizontalAlignment;
-    private final short verticalAlignment;
-    private final boolean italic;
+    private final short               height;
+    private final boolean             boldWeight;
+    private final byte                underline;
+    private final HorizontalAlignment horizontalAlignment;
+    private final VerticalAlignment   verticalAlignment;
+    private final boolean             italic;
 
-	private FontStyle(Integer height, short boldWeight, byte underline, short horizontalAlignment,
-                      short verticalAlignment) {
+    FontStyle(Integer height, boolean boldWeight, byte underline, HorizontalAlignment horizontalAlignment,
+              VerticalAlignment verticalAlignment) {
         this(height, boldWeight, underline, horizontalAlignment, verticalAlignment, false);
-	}
+    }
 
-    private FontStyle(Integer height, short boldWeight, byte underline, short horizontalAlignment,
-                      short verticalAlignment, boolean italic) {
+    FontStyle(Integer height, boolean boldWeight, byte underline, HorizontalAlignment horizontalAlignment,
+              VerticalAlignment verticalAlignment, boolean italic) {
         this.height = height.shortValue();
         this.boldWeight = boldWeight;
         this.underline = underline;
@@ -49,22 +49,22 @@ public enum FontStyle implements AdditiveStyle {
         this.italic = italic;
     }
 
-	@Override
-	public void enrich(WorkbookContext workbookContext, org.apache.poi.ss.usermodel.CellStyle style) {
+    @Override
+    public void enrich(WorkbookContext workbookContext, org.apache.poi.ss.usermodel.CellStyle style) {
         Font font = workbookContext.toNativeWorkbook().createFont();
         font.setFontName(workbookContext.getDefaultFontName());
         font.setFontHeightInPoints(height);
-        font.setBoldweight(boldWeight);
+        font.setBold(boldWeight);
         font.setUnderline(underline);
         font.setItalic(italic);
         style.setFont(font);
 
         style.setAlignment(horizontalAlignment);
         style.setVerticalAlignment(verticalAlignment);
-	}
+    }
 
-	@Override
-	public Enum<StyleType> getType() {
-		return StyleType.FONT;
-	}
+    @Override
+    public Enum<StyleType> getType() {
+        return StyleType.FONT;
+    }
 }
